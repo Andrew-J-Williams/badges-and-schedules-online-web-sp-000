d01576155e05d3d1b9bf1049aa0e726ce4cd5a46
Andrew Williams <1williamsaj@gmail.com>
2019-08-31 20:18:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ def batch_badge_creator(array)
 end
 
 
-def assign_rooms(array,number)
+def assign_rooms(array, number)
 
   array.each do |room|
     return "Hello, #{room}! You'll be assigned to room #{number}!"
